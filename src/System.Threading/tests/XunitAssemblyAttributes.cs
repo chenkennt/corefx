7c663b87374a9ac845d9784640b71a6980ad4a49
Ken Chen <kenchen@microsoft.com>
2015-07-18 18:18:58 -0700

    Add doc to git pages

--- src/System.Threading/tests/XunitAssemblyAttributes.cs
+++
@@ -1,7 +0,0 @@
-// Copyright (c) Microsoft. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using Xunit;
-
-// System.Threading tests are not designed to be run in parallel.
-[assembly: CollectionBehavior(CollectionBehavior.CollectionPerAssembly, DisableTestParallelization = true, MaxParallelThreads = 1)]
